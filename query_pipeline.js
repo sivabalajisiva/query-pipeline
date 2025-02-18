6c62a382fb1b10f62517ef52d18de45d60966fce
sivabalajisiva <150930405+sivabalajisiva@users.noreply.github.com>
2025-02-18 08:21:28 +0530

    Update query_pipeline.js

--- query_pipeline.js
+++ query_pipeline.js
@@ -440,55 +440,7 @@ Recommended when using async/await for cleaner and more readable code.
 
 
 
--------------------------------------------------
-2, how many category user select this category?
-  
- db.orders.aggregate([
-    {  $match: { Categoryis: "abc"  } },
-    {  $count: "value" }
-])
-  
-// value 3
---------------------------------------------------
-3, price list la 50 ku mela erukura price only show akum
-  // [ { "_id": 1, "name": "Laptop", "price": 1200 },
-  // { "_id": 2, "name": "Smartphone", "price": 800 },
-  // { "_id": 3, "name": "Headphones", "price": 100 },
-  // { "_id": 4, "name": "Mouse", "price": 25 },
-  // { "_id": 5, "name": "Keyboard", "price": 60 },
-  // { "_id": 6, "name": "Monitor", "price": 200 },
-  // { "_id": 7, "name": "Charger", "price": 50 }]
-db.products.aggregate([
-  { $match: { price: { $gt: 50 } } }, // Filter products with price > 50
-  { $sort: { price: -1 } }           // Sort by price descending
-])
-// [{ "_id": 1, "name": "Laptop", "price": 1200 },
-//   { "_id": 2, "name": "Smartphone", "price": 800 },
-//   { "_id": 6, "name": "Monitor", "price": 200 },
-//   { "_id": 3, "name": "Headphones", "price": 100 },
-//   { "_id": 5, "name": "Keyboard", "price": 60 }]
-----------------------------------------------------
-4,How do you calculate the total sales and average sales in the same pipeline?
-  // [{ "_id": 1, "product": "Laptop", "amount": 1200 },
-  // { "_id": 2, "product": "Smartphone", "amount": 800 },
-  // { "_id": 3, "product": "Headphones", "amount": 100 },
-  // { "_id": 4, "product": "Mouse", "amount": 50 },
-  // { "_id": 5, "product": "Keyboard", "amount": 150 }]
-db.sales.aggregate([
-  {
-    $group: {
-      _id: null,
-      totalSales: { $sum: "$amount" },
-      avgSales: { $avg: "$amount" }
-    }
-  }
-])
-// [{
-//     "_id": null,
-//     "totalSales": 2300,        // 1200 + 800 + 100 + 50 + 150
-//     "avgSales": 460            // (2300 / 5)
-// }]
-----------------------------------------------------
+
 5,$addFields
 // [{  _id: 1, student: "Maya",  homework: [ 10, 5, 10 ], quiz: [ 10, 8 ], extraCredit: 0 },
 //    {  _id: 2, student: "Ryan", homework: [ 5, 6, 5 ],  quiz: [ 8, 8 ],  extraCredit: 8 }]
@@ -783,5 +735,53 @@ exports.graphlookup = async (req,res) =>{
 }
 
 
-
+-------------------------------------------------
+2, how many category user select this category?
+  
+ db.orders.aggregate([
+    {  $match: { Categoryis: "abc"  } },
+    {  $count: "value" }
+])
+  
+// value 3
+--------------------------------------------------
+3, price list la 50 ku mela erukura price only show akum
+  // [ { "_id": 1, "name": "Laptop", "price": 1200 },
+  // { "_id": 2, "name": "Smartphone", "price": 800 },
+  // { "_id": 3, "name": "Headphones", "price": 100 },
+  // { "_id": 4, "name": "Mouse", "price": 25 },
+  // { "_id": 5, "name": "Keyboard", "price": 60 },
+  // { "_id": 6, "name": "Monitor", "price": 200 },
+  // { "_id": 7, "name": "Charger", "price": 50 }]
+db.products.aggregate([
+  { $match: { price: { $gt: 50 } } }, // Filter products with price > 50
+  { $sort: { price: -1 } }           // Sort by price descending
+])
+// [{ "_id": 1, "name": "Laptop", "price": 1200 },
+//   { "_id": 2, "name": "Smartphone", "price": 800 },
+//   { "_id": 6, "name": "Monitor", "price": 200 },
+//   { "_id": 3, "name": "Headphones", "price": 100 },
+//   { "_id": 5, "name": "Keyboard", "price": 60 }]
+----------------------------------------------------
+4,How do you calculate the total sales and average sales in the same pipeline?
+  // [{ "_id": 1, "product": "Laptop", "amount": 1200 },
+  // { "_id": 2, "product": "Smartphone", "amount": 800 },
+  // { "_id": 3, "product": "Headphones", "amount": 100 },
+  // { "_id": 4, "product": "Mouse", "amount": 50 },
+  // { "_id": 5, "product": "Keyboard", "amount": 150 }]
+db.sales.aggregate([
+  {
+    $group: {
+      _id: null,
+      totalSales: { $sum: "$amount" },
+      avgSales: { $avg: "$amount" }
+    }
+  }
+])
+// [{
+//     "_id": null,
+//     "totalSales": 2300,        // 1200 + 800 + 100 + 50 + 150
+//     "avgSales": 460            // (2300 / 5)
+// }]
+----------------------------------------------------
 
